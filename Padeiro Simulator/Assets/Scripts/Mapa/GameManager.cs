ecd78d8e6090df1312302c6fba5138831c253d95
Gustavo Lima dos Santos <gugolho@gmail.com>
2022-07-06 15:44:15 -0300

    Salving

--- Padeiro Simulator/Assets/Scripts/Mapa/GameManager.cs
+++ Padeiro Simulator/Assets/Scripts/Mapa/GameManager.cs
@@ -16,19 +16,4 @@ public class GameManager : MonoBehaviour
         
     }
     
-    public GameObject PegandoItem(bool aleatorio = false, int n1 = 0)
-    {
-        var preFab = Resources.Load<GameObject>("Comida");
-        var item = Instantiate(preFab, Vector3.zero, Quaternion.identity);
-
-        if (aleatorio)
-        {
-            n1 = Random.Range(0, 6);
-
-            item.GetComponent<ComidaController>().QualComida(n1);
-        }
-
-        return Instantiate(preFab, Vector3.zero, Quaternion.identity);
-    }
-    
 }
