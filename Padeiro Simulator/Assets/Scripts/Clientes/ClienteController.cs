a359342e2ec5d7318dbe67cd8a269467671dd4ea
Gustavo Lima dos Santos <gugolho@gmail.com>
2022-07-06 11:12:08 -0300

    Fazendo o pedido
    agora, ele só pega as variáveis de uma bancada, e depois de sair dela ela reseta, e ele tbm podendo assim fazer outro pedido

--- Padeiro Simulator/Assets/Scripts/Clientes/ClienteController.cs
+++ Padeiro Simulator/Assets/Scripts/Clientes/ClienteController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ClienteController : MonoBehaviour
 {
     //Variaveis para encontrar a bancada
+    [SerializeField] private GameObject qualBancada;
     [SerializeField] private Transform ondeVai;
     [SerializeField] private bool bancaGO;
 
@@ -13,6 +14,9 @@ public class ClienteController : MonoBehaviour
     [SerializeField] private Sprite[] clientes;
     [Range(0, 2)]
     [SerializeField] private int quem;
+
+    //Caracteristicas do cliente
+    [SerializeField] private float vel = 5f;
 
     // Start is called before the first frame update
     void Start()
@@ -25,30 +29,49 @@ public class ClienteController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //Localizei a bancada e aqui faz ele ir até ela
         if (bancaGO && ondeVai != null)
         {
-            transform.position = ondeVai.position;
+            //Fazendo ele se mover para a bancada
+            transform.position = new Vector3(Mathf.Lerp(transform.position.x, ondeVai.position.x, vel * Time.deltaTime / 5),
+                                            Mathf.Lerp(transform.position.y, ondeVai.position.y, vel * Time.deltaTime / 5), 0);
+            
             bancaGO = true;
         }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Bancada"))
+        if (collision.gameObject.CompareTag("Bancada") && !bancaGO)
         {
+            //pegando o gameObject da bancada
+            qualBancada = collision.gameObject;
+
             //Pegando as variaveis da bancada para saber onde fazer o pedido
-            bancaGO = GetComponent<BancadaController>().TenhoCliente();
+            bancaGO = qualBancada.GetComponent<BancadaController>().TenhoCliente();
 
             //Se tiver alguém não posso ir
             if (!bancaGO)
             {
+
                 bancaGO = true;
-                ondeVai = GetComponent<BancadaController>().LugarPedir();
+                ondeVai = qualBancada.GetComponent<BancadaController>().LugarPedir();
             } else
             {
                 bancaGO = false;
             }
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Bancada") && bancaGO)
+        {
+            //Retirando as configue da bancada que devo ir
+            qualBancada.GetComponent<BancadaController>().TenhoCliente(true);
+            qualBancada = null;
+            bancaGO = false;
+            ondeVai = null;
+            
         }
     }
 }
--- Padeiro Simulator/Assets/Scripts/Mapa/BancadaController.cs
+++ Padeiro Simulator/Assets/Scripts/Mapa/BancadaController.cs
@@ -19,14 +19,20 @@ public class BancadaController : MonoBehaviour
         
     }
 
-    public bool TenhoCliente()
+    //Entregando a variavel para o cliente, se ele definir reste como true, a bancada voltara a não ter cliente
+    public bool TenhoCliente(bool reset = false)
     {
+        //Se eu não tenho cliente, vou definir que agora tenho, e vou falar que o cliente que me solicitou pode vir
         if (!tenhoCliente)
         {
             tenhoCliente = true;
             return false;
         }
-
+        //Se definiram reset como true, é pq o cliente já fez o pedido e agora pode entrar outro
+        if (reset)
+        {
+            tenhoCliente = false;
+        }
         return tenhoCliente;
     }
     public Transform LugarPedir()
