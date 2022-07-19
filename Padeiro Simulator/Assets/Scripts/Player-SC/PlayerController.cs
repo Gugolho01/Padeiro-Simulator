bf3635fc5348d1be479f3db8471284df7733809e
Gustavo Lima dos Santos <92238035+Gugolho01@users.noreply.github.com>
2022-07-19 15:15:38 -0300

    Troca de sprite da sacola
    fiz o mesmo sistema da comida para a sacola, só muda o jeito que chama a função

--- Padeiro Simulator/Assets/Scripts/Player-SC/PlayerController.cs
+++ Padeiro Simulator/Assets/Scripts/Player-SC/PlayerController.cs
@@ -1,15 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
-    
     private Animator meuAnim;
 
     [SerializeField] private float speed;
+
+    //Verificando se está segurando um item
     [SerializeField] private Transform naMaoObj;
     [SerializeField] private bool segurando;
+
+    //Variaveis do inventory
+    [SerializeField] private List<GameObject> productos;
 
     // Start is called before the first frame update
     void Start()
@@ -20,8 +25,21 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
         Movendo();
         CarregaItem();
+
+        //Guardando item na sacola
+        if (Input.GetKeyUp(KeyCode.R) && segurando && naMaoObj != null)
+        {
+            productos.Add(naMaoObj.gameObject);
+            //Destroy(naMaoObj.gameObject);
+            //GetComponent<SacolaController>().MudaImageSacola();
+
+            //resetando coisas na mão do player
+            naMaoObj = null;
+            segurando = false;
+        }
     }
 
     private void CarregaItem()
---
+++ Padeiro Simulator/Assets/Scripts/Player-SC/SacolaController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SacolaController : MonoBehaviour
+{
+    [SerializeField] private Sprite myImagePrincipal;
+    [SerializeField] private Image myImage;             //Sprite que vai mostar
+    [SerializeField] private Sprite[] sacolaComidas;    //Conjunto de imagens
+    [SerializeField] private int qualComida;            //Numero da imagem
+
+    [SerializeField] private bool tenhoComida;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (tenhoComida) 
+        {
+            myImage = GetComponent<Image>();
+            myImage.sprite = sacolaComidas[qualComida];
+        } else
+        {
+            myImage = GetComponent<Image>();
+            myImage.sprite = myImagePrincipal;
+        }
+    }
+
+    public void MudaImageSacola()
+    {
+        Debug.Log("rodei");
+        myImage = GetComponent<Image>();
+        myImage.sprite = sacolaComidas[qualComida];
+
+    }
+}
